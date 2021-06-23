d9d12e21fb85c8ece7f33f7b46ebc14539c5cbe7
Ken Lee <kdblitz@gmail.com>
2021-06-23 17:40:11 +0000

    fix s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,6 @@ module "s3_bucket" {
   source  = "app.terraform.io/klee-training/s3-bucket/aws"
   version = "2.2.0"
 
-  bucket = "my-s3-bucket"
   acl    = "private"
   bucket_prefix = var.prefix
 
